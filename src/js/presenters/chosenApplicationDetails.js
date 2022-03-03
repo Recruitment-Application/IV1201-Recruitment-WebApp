e44d7c4b8d724db17ed75008baa66d03110cc89d
Garo <garo.malko222@gmail.com>
2022-03-03 21:31:43 +0100

    rendering NoApplicantAccessView and FailedSignInView have been corrected
    rendering NoApplicantAccessView and FailedSignInView have been corrected

--- src/js/presenters/chosenApplicationDetails.js
+++ src/js/presenters/chosenApplicationDetails.js
@@ -7,21 +7,18 @@ import FailedSignInView from "../views/failedSignInView";
 
 function ChosenApplicationDetails({ userModel, applicationModel, navToApplicationsList }) {
 
-  //console.log(1);
+
   const [decision, setDecision] = React.useState("");
 
-  const signedIn = true;
-
-  // const tempapplicationData = ["Garo", "Malko", "Ticket Seller", "17/2/2022", "20/2/2022", 5, 2];
   const decisionList = ["Accepted", "Rejected", "Unhandled"];
+
+  let applicationId = useModelProp(applicationModel, "currentApplicationID");
+  let chosenApplicationDetails = useModelProp(applicationModel, "chosenApplicationData");
 
   let modelRoleId = useModelProp(userModel, "role");
 
-  let applicationId = useModelProp(applicationModel, "currentApplicationID");
-  let chosenApplicationDetails = useModelProp(applicationModel, "chosenApplicationData");;
-
-
   if (modelRoleId === RolesEnum.Recruiter) {
+
     /**
      * send the required data to render the ChosenApplicationDetailsView and return an action for the made decision by the recruiter.
      */
@@ -39,10 +36,10 @@ function ChosenApplicationDetails({ userModel, applicationModel, navToApplicatio
     );
   }
   else if (modelRoleId === RolesEnum.Applicant) {
-    React.createElement(NoApplicantAccessView, {})
+   return React.createElement(NoApplicantAccessView, {});
   }
   else {
-    React.createElement(FailedSignInView, {})
+   return React.createElement(FailedSignInView, {});
   }
 }
 export default ChosenApplicationDetails;
