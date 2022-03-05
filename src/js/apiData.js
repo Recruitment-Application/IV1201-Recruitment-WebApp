422aa7bf15b984900a711e0374dc0d83c2e9b102
Gleano Malke <gleano.100@hotmail.com>
2022-03-05 15:17:22 +0100

    Added comments to the functionality part in the code
    Added comments to the functionality part in the code.

--- src/js/apiData.js
+++ src/js/apiData.js
@@ -1,35 +1,38 @@
 import BASE_URL from "./apiConfig";
 
+/**
+ * Responsible for the api calls that will happen in the website. Ranging from creating the 
+ * proper request to receiving the response.
+ */
 const ApiData = {
     /**
-     * the base apiCall function that fetch the BASE_URL, the sent urlPath according to each function 
-     * and the requestContent contains required inputs differs from a function to another.
-     * fetch function is a used to request to a server specified in the ./apiConfig URL running at the same time parallel with the webapp npm.
-     * fetch also load the information in the webpages.
-     * @param {*} urlPath passed url by function while calling apiCall. 
-     * @param {*} requestContent the requestedContent by function while calling apiCall. 
-     *                           it will contain method type, credentials, headers and body.
-     * @returns returns teh response.
+     * The base api call function that fetches resources using the BASE_URL, and the specified urlPath 
+     * according to the needed functionality to be preformed. 
+     * fetch function is using a base URL for the requests that is the hosted server, 
+     * the information about the host server is specified in the apiConfig.js.
+     * @param {string} urlPath Specific URL related to the api for retrieving specific recourses.
+     * @param {Request} requestContent The request content needed for the request to fetch the info. 
+     *                                 it will contain method type, credentials, headers and body.
+     * @returns {Response} The response from the sent request.
+     * @throws {Exception} Any type of error that could be encountered through the call.
      */
     apiCall(urlPath, requestContent) {
         const fetchData = fetch(BASE_URL + urlPath, requestContent)
-            .then((response) => {
-
-                return response;
-            })
-            .catch((error) => {throw error;});
+            .then((response) => { return response; })
+            .catch((error) => { throw error; });
         return fetchData;
-
     },
 
     /**
-     * signinUser function passes the username and password as a body content to the user/signin path to siginin to the specified user account.
-     * @param {*} username the entered username
-     * @param {*} password the entered password
-     * @returns returns the reponse after signing in. 
+     * Create the proper request with the user info to try to sign in 
+     * the user into the account.
+     * user/signin is the endpoint that the api call will answer to for 
+     * signing in users.
+     * @param {string} username The username of the account.
+     * @param {string} password The password of the account.
+     * @returns {Response} The response after signing in. 
      */
     signinUser(username, password) {
-
         return this.apiCall(
             'user/signin'
             , {
@@ -39,8 +42,10 @@ const ApiData = {
                     'Accept': 'application/json, text/plain, */*',
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ "username": username, "password": password }),
-
+                body: JSON.stringify({
+                    "username": username,
+                    "password": password
+                }),
             }
         ).then((response) => {
             return response;
@@ -48,11 +53,12 @@ const ApiData = {
     },
 
     /**
-     * checkSignin function checks if the user is still signed in according to the stored cookies. 
-     * @returns return the repsonse of the apicall.
+     * Checks if the user is still signed in according to the stored cookie.
+     * user/checkSignin is the endpoint that the api call will answer to for
+     * checking the sing in validation. 
+     * @returns {Response} The response after the signin check.
      */
     checkSignin() {
-
         return this.apiCall(
             'user/checkSignin'
             , {
@@ -68,8 +74,10 @@ const ApiData = {
     },
 
     /**
-     * signout the user from the webpage.
-     * @returns return the repsonse of the apiCall.
+     * Sign out the user that was signed in before.
+     * user/signout is the endpoint that the api call will answer to for
+     * signing out the user.
+     * @returns {Response} The response after signing out. 
      */
     signoutUser() {
         return this.apiCall(
@@ -87,18 +95,18 @@ const ApiData = {
     },
 
     /**
-     * The function signup a new user to the database and keep the user signedin after signin up on the webpage. 
-     * Send the entered data as a body content in JSON.stringify({})
-     * @param {*} firstName the entered user's firstName
-     * @param {*} lastName the entered user's lastName
-     * @param {*} personNumber the entered user's personNumber
-     * @param {*} email the entered user's email
-     * @param {*} username the entered user's username
-     * @param {*} password  the entered user's password
-     * @returns return the repsonse of the apiCall.
+     * Try to create a new account for the new user. Providing the needed info about the new user.
+     * user/signup is the endpoint that the api call will answer to for
+     * signing up the user with a new account.
+     * @param {string} firstName The first name of the new user.
+     * @param {string} lastName The last name of the new user.
+     * @param {string} personNumber The personal number of the new user.
+     * @param {string} email The email of the new user.
+     * @param {string} username the username for the new account.
+     * @param {string} password The password for the new account.
+     * @returns {Response} The response after trying to create the new account.
      */
     signupUser(firstName, lastName, personNumber, email, username, password) {
-
         return this.apiCall(
             'user/signup'
             , {
@@ -109,10 +117,30 @@ const ApiData = {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    "firstname": firstName, "lastname": lastName,
-                    "personalNumber": personNumber, "email": email, "username": username, "password": password
+                    "firstname": firstName,
+                    "lastname": lastName,
+                    "personalNumber": personNumber,
+                    "email": email,
+                    "username": username,
+                    "password": password
                 }),
+            })
+            .then((response) => {
+                return response;
+            });
+    },
 
+    /**
+     * Get the jobs that are available.
+     * job/getJobs is the endpoint that the api call will answer to for getting the jobs.
+     * @returns {Response} The response after getting the available jobs.
+     */
+    getJobs() {
+        return this.apiCall(
+            'job/getJobs'
+            , {
+                method: 'get',
+                credentials: 'include',
             }
         )
             .then((response) => {
@@ -120,34 +148,50 @@ const ApiData = {
             });
     },
 
-    getJobs() {
-        //http://127.0.0.1:3030/job/getJobs
-        return this.apiCall(
-            'job/getJobs'
-            , {
-                method: 'get',
-                credentials: 'include',
-            }
-        ).then((response) => {
-            return response;
-        });
-    },
-
+    /**
+     * List the application that could be filtered with different values that are the parameters. 
+     * The user must be a recruiter.
+     * job/listApplications is the endpoint that the api call will answer to for
+     * getting the application that are following the filtering parameters if they exist.
+     * There are specific values for the parameters indicating if they exist.
+     * @param {string} name The name of the applicant that the application relates to.
+     *                      Leaving it empty means no filtering with name.
+     * @param {number} competenceId The competence that is related to the application filtering.
+     *                              Leaving it with 0 value means no filtering with competence.
+     * @param {string} dateFrom The start date that the user is available.
+     *                          Leaving it empty means no filtering with dateFrom.
+     * @param {string} dateTo The end date that the user is available.
+     *                        Leaving it empty means no filtering with dateTo.
+     * @param {number} page The page that the list is needed to get.
+     *                      Leaving it with 0 value means all the applications.
+     * @returns {Response} The response after listing the application that maybe were filtered.
+     */
     listApplications(name, competenceId, dateFrom, dateTo, page) {
         return this.apiCall(
             'job/listApplications?' + new URLSearchParams({
-                "name": name, "competenceId": competenceId,
-                "dateFrom": dateFrom, "dateTo": dateTo, "page": page
+                "name": name,
+                "competenceId": competenceId,
+                "dateFrom": dateFrom,
+                "dateTo": dateTo,
+                "page": page
             })
             , {
                 method: 'get',
                 credentials: 'include',
             }
-        ).then((response) => {
-            return response;
-        });
+        )
+            .then((response) => {
+                return response;
+            });
     },
 
+    /**
+     * Get all needed information about an application. The user must be a recruiter.
+     * job/getApplication is the endpoint that the api call will answer to for
+     * getting the information desired about the application.
+     * @param {number} applicationId The id for the specified application.
+     * @returns {Response} The response after trying to retrieve the application info.
+     */
     getApplicationDetails(applicationId) {
         return this.apiCall(
             'job/getApplication?' + new URLSearchParams({
@@ -157,14 +201,23 @@ const ApiData = {
                 method: 'get',
                 credentials: 'include',
             }
-        ).then((response) => {
-            return response;
-        });
+        )
+            .then((response) => {
+                return response;
+            });
     },
 
-
+    /**
+     * Register a new application for the user with the needed information. The user must be an applicant.
+     * job/registerApplication is the endpoint that the api call will answer to for
+     * registering the application for the application(user).
+     * @param {number} competenceId The competence id that is related to the job.
+     * @param {number} yearsOfExperience The amount of experience that the applicant has in the job.
+     * @param {string} dateFrom The start date that the user is available.
+     * @param {string} dateTo The end date that the user is available
+     * @returns {Response} The response after trying to register the new application for the applicant.
+     */
     submitApplication(competenceId, yearsOfExperience, dateFrom, dateTo) {
-
         return this.apiCall(
             'job/registerApplication'
             , {
@@ -175,20 +228,27 @@ const ApiData = {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    "competenceId": competenceId, "yearsOfExperience": yearsOfExperience,
-                    "dateFrom": dateFrom, "dateTo": dateTo
+                    "competenceId": competenceId,
+                    "yearsOfExperience": yearsOfExperience,
+                    "dateFrom": dateFrom,
+                    "dateTo": dateTo
                 }),
-
             }
         )
             .then((response) => {
                 return response;
             });
-
     },
 
+    /**
+     * Take a decision about an application and submit the decision. The user must be a recruiter.
+     * job/submitDecision is the endpoint that the api call will answer to for
+     * submitting the decision about the application.
+     * @param {number} applicationId The id of the application that the decision is related to.
+     * @param {string} decision The decision taken about the application.
+     * @returns {Response} The response after trying to submit the decision about an application.
+     */
     submitApplicationDecision(applicationId, decision) {
-
         return this.apiCall(
             'job/submitDecision'
             , {
@@ -198,16 +258,16 @@ const ApiData = {
                     'Accept': 'application/json, text/plain, */*',
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ "applicationId": applicationId, "decision": decision }),
-
+                body: JSON.stringify({
+                    "applicationId": applicationId,
+                    "decision": decision
+                }),
             }
-        ).then((response) => {
-            return response;
-        });
+        )
+            .then((response) => {
+                return response;
+            });
     },
-
-
-
 };
 
 export default ApiData;
--- src/js/useModelProp.js
+++ src/js/useModelProp.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+/**
+ * Responsible to observe specific attributes of a class.
+ * @param {Class} model The class that is desired to observe on.
+ * @param {string} prop The property name tha is desired to observe on.
+ * @returns {any} Get the new value after any changes.
+ */
 function useModelProp(model, prop) {
   const [propValue, setPropValue] = React.useState(model[prop]);
   React.useEffect(
